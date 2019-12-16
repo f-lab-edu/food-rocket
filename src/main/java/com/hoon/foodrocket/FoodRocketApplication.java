0d7f193c8c34685b9384fac09efd7488aa983c49
seohoon-dev <sh931123@gmail.com>
2019-12-16 17:47:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/hoon/foodrocket/FoodRocketApplication.java
@@ -0,0 +1,13 @@
+package com.hoon.foodrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoon/foodrocket/FoodRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon.foodrocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
